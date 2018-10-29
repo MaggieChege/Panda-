aa05086001fbfeb5361f8cbc5ac7379bf965d61c
Joma Simeon <simjoms@gmail.com>
2018-10-29 12:54:55 +0300

    [ft-161541298] add user view for creating account

--- app/api/v2/views.py
+++ app/api/v2/views.py
@@ -1,14 +1,17 @@
 from  flask_restful import Resource
 from flask import jsonify, make_response, request
+from app.api.v2.models import UserModels
 
 class User(Resource):
     def post(self):
         data=request.get_json()
+        email=data['email']
+        password=data['password']
 
-    try:
-    
-
-
-    except Exception as e:
-        print(e)
+        try:
+            user=UserModels(email,password)
+            user.save()
+            return make_response(jsonify({"message": "user created successfully"}), 201)
+        except Exception as e:
+            print(e)
        
