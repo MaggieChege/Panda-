09b9b26eee905e8ae2e0ca1d37ff11b48c0b3b13
Too Collins <collinstoo.ct@gmail.com>
2018-10-29 13:33:19 +0300

    [Chore 161541298]  Refactore user view to pass tests

--- app/tests/user_test.py
+++ app/tests/user_test.py
@@ -1,8 +1,8 @@
 from app.tests.base_test import BaseTestCase
-
+import json
 class User(BaseTestCase):
     def test_create_account(self):
         user={"email":"jane@gmail.com","password":"123456"}
-        response=self.post("/user",user)
-
+        response=self.post("/api/v2/user",user)
         self.assertEqual(response.status_code,201)
+        self.assertEqual({"message": "user created successfully"},json.loads(response.data))
