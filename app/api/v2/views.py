f9b0e825cad22fd19a31a692a3b74024bae498b5
Joma Simeon <simjoms@gmail.com>
2018-10-29 12:43:46 +0300

    [ft-161541298] add user view

--- app/api/v2/views.py
+++ app/api/v2/views.py
@@ -1,1 +1,14 @@
-from flask import flask_restful
+from  flask_restful import Resource
+from flask import jsonify, make_response, request
+
+class User(Resource):
+    def post(self):
+        data=request.get_json()
+
+    try:
+    
+
+
+    except Exception as e:
+        print(e)
+       
